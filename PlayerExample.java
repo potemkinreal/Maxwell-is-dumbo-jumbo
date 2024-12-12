427e86bdebf089b90510eafc948f6869b2099229
potemkinreal <ej7313@pleasantonusd.net>
2024-12-12 14:39:52 -0800

    Made my strategy, absolute randomness

--- PlayerExample.java
+++ PlayerExample.java
@@ -21,9 +21,20 @@ public class PlayerExample implements Player
      *              "s" - scissors
      *              anything else - forfeit the turn
      */
+    private int tommy = 0;
     public String move(String [] myMoves, String [] opponentMoves, int myScore, int opponentScore)
     {
-        return " ";
+    //
+        if (tommy % 3 ==0 || tommy == 31){
+            return "r";
+        }
+        if (tommy*2 == 15 || tommy%2 ==0){
+            return "s";
+        }
+         if (tommy%5 ==0){
+            return "p";
+        tommy +=1;
+        return "p";
     }
     /**
      * Returns the name of the player
